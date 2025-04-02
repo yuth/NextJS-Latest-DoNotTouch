3b960ac4d00441853cdf7b82393d23942f8247be
Yathi <511386+yuth@users.noreply.github.com>
2025-04-02 10:18:39 -0700

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,6 @@ const nextConfig = {
       }
     ],
     formats: ['image/avif', 'image/webp'],
-    placeholder: 'blur',
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 7680, 5000],
   },
   async headers() {
